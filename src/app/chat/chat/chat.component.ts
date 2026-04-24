8af1af54e3d2afdb2ece27f1624c7fbd39030cd1
Robert Wyszynski <robertointerwento@gmail.com>
2026-04-24 22:01:04 +0200

    add chat page

--- src/app/chat/chat/chat.component.ts
+++ src/app/chat/chat/chat.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule], // 👈 TO JEST KLUCZ
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css'
+  styleUrls: ['./chat.component.css']
 })
 export class ChatComponent {
+  users = [
+    { id: 1, email: 'user1@test.com' },
+    { id: 2, email: 'user2@test.com' }
+  ];
 
+  messages: any[] = [];
+  selectedUser: any = null;
+  newMessage = '';
+
+  selectUser(user: any) {
+    this.selectedUser = user;
+    this.messages = [];
+  }
+
+  send() {
+    if (!this.newMessage.trim()) return;
+
+    this.messages.push({
+      text: this.newMessage,
+      me: true
+    });
+
+    this.newMessage = '';
+  }
 }
